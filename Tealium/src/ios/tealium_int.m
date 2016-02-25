60f49d6e7ce6d37d4415b58e75a99452acdeb7da
Craig Rouse <craig@tealium.com>
2016-02-25 14:01:50 +0000

    Committing new plugin code
    Updating sample app
    
    Updated Readme
    
    Updated readme
    
    Clean build of test apps for iOS and Android
    
    Added extra error checking to Android java plugin
    
    Minor tweaks, but awesome otherwise!
    
    Removed unnecessary vars in android plugin
    
    Some minor updates to the README
    
    More doc updates
    
    Updated the sample app with some comments
    
    Minor doc update
    
    a few updates to the sample app
    
    minor update to sample app
    
    recompiled sample apps
    
    minor updates to readme
    
    A few updates
    
    - Removed commented line from plugin.xml
    - Added error checking to tealium_int.m for iOS
    - Added gitignore for platforms directory
    - Removed auto_track.js (no longer required)
    
    Updated gitignore
    
    Removed auto_track.js from plugin.xml
    
    Updated README.md with some build instructions
    
    Added error logging via javascript plugin
    
    Very minor update to readme

--- Tealium/src/ios/tealium_int.m
+++ Tealium/src/ios/tealium_int.m
@@ -1,11 +1,11 @@
 #import "tealium_int.h"
-#import <TealiumLibrary/Tealium.h>
 #import <Cordova/CDV.h>
+@import TealiumIOS;
 
 @implementation TealiumPg
 
 static NSString * const Tealium_Platform = @"ios_cordova";
-static NSString * const Tealium_LibVersion = @"4.1.10c";
+static NSString * const Tealium_LibVersion = @"5.0.1";
 static NSString * const Tealium_MobileBaseURL = @"https://tags.tiqcdn.com/utag/%@/%@/%@/mobile.html?%@=%@&%@=%@&%@=%@&%@=%@";
 
 - (void) init: (CDVInvokedUrlCommand*)command {
@@ -14,67 +14,62 @@ static NSString * const Tealium_MobileBaseURL = @"https://tags.tiqcdn.com/utag/%
     NSString* accountName;
     NSString* profileName;
     NSString* environmentName;
-    NSDictionary* customData = nil;
-	int options;
-	
+    NSString* instanceName;
+    // Below functions did not work as regular methods during Cordova build - added inline below
+    NSTimeInterval ti = [[NSDate date] timeIntervalSince1970];
+    long tiLong = (long)ti;
+    NSString *timestamp = [NSString stringWithFormat:@"%li", tiLong];
+    NSString *osVersion = [[UIDevice currentDevice] systemVersion];
+
 	@try {
 	    arguments = [command.arguments objectAtIndex:0];
 	    accountName = [arguments objectForKey:@"account"];
 	    profileName = [arguments objectForKey:@"profile"];
 	    environmentName = [arguments objectForKey:@"environment"];
-		
-		id disableHTTPSVal = [arguments objectForKey:@"disableHTTPS"];
-		id suppressLogsVal = [arguments objectForKey:@"suppressLogs"];
-        id disableExceptionHandlingVal = [arguments objectForKey:@"disableExceptionHandling"];
-		
-		BOOL disableHTTPS = disableHTTPSVal == nil ? NO : [disableHTTPSVal boolValue];
-		BOOL suppressLogs = suppressLogsVal == nil ? YES : [suppressLogsVal boolValue];
-        BOOL disableExceptionHandling = disableExceptionHandlingVal == nil ? NO : [disableExceptionHandlingVal boolValue];
-				
-		options = 
-			(disableHTTPS ? TLDisableHTTPS : 0) |
-			(suppressLogs ? TLSuppressLogs : TLDisplayVerboseLogs) |
-			(disableExceptionHandling ? disableExceptionHandling : 0);
-        
-        // Below functions did not work as regular methods during Cordova build - added inline below
-        
-        // timestamp
-        NSTimeInterval ti = [[NSDate date] timeIntervalSince1970];
-        long tiLong = (long)ti;
-        NSString *timestamp = [NSString stringWithFormat:@"%li", tiLong];
-        
-        // mobile url override
-        NSString *osVersion         = [[UIDevice currentDevice] systemVersion];
-        NSString *mobileURLString = [NSString stringWithFormat:Tealium_MobileBaseURL,accountName, profileName, environmentName, TealiumDSK_Platform, Tealium_Platform,  TealiumDSK_OSVersion, osVersion, TealiumDSK_LibraryVersion, Tealium_LibVersion, TealiumDSK_TimestampUnix, timestamp];
-        
-        customData = @{TealiumDSK_OverrideUrl : mobileURLString};
-			
+        instanceName = [arguments objectForKey:@"instance"];
 	}
 	@catch (NSException *exception) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         return;
 	}
-    
-    if (accountName != nil && [accountName length] > 0) {
-        [Tealium initSharedInstance: accountName
-                            profile: profileName
-                             target: environmentName
-							options: options
-                   globalCustomData: customData];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Tealium init"];
+
+    if (accountName == nil || (accountName != nil && [accountName length] == 0)) {
+        // account name not valid - set error response
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Tealium: Account Name is null or empty. Please provide a valid account name."];
+    } else if (instanceName == nil || (instanceName != nil && [instanceName length] == 0)) {
+        // instance name not valid - set error response
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Tealium: Instance Name is null or empty. Please provide a valid instance name."];
+    } else if (profileName == nil || (profileName != nil && [profileName length] == 0)) {
+        // profile name not valid - set error response
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Tealium: Profile Name is null or empty. Please provide a valid profile name."];
     } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        // we got here with syntactically valid account/profilename/instancename - attempt to init Tealium
+        // generate mobile.html override string
+        NSString *mobileURLString = [NSString stringWithFormat:Tealium_MobileBaseURL, accountName, profileName, environmentName, TEALDataSourceKey_Platform, Tealium_Platform, TEALDataSourceKey_DeviceOSVersion, osVersion, TEALDataSourceKey_LibraryVersion, Tealium_LibVersion, TEALDataSourceKey_TimestampUnix, timestamp];
+        // create a new config object
+        TEALConfiguration *configuration = [TEALConfiguration configurationWithAccount:accountName
+                                                                            profile:profileName
+                                                                            environment:environmentName];
+        // set override for mobile.html - publish settings and web view
+        configuration.overrideTagManagementURL = configuration.overridePublishSettingsURL = mobileURLString;
+        // create a new Tealium instance using the instance name passed in
+        [Tealium newInstanceForKey:instanceName configuration:configuration];
+        // set success response - we initialized successfully with no reported errors
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Tealium: Tealium init successful!"];
     }
     
+    // send the plugin response - success or failure
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    
 }
 
 -  (void) track:(CDVInvokedUrlCommand*)command {
     CDVPluginResult* pluginResult = nil;
     NSDictionary* arguments;
     NSString* eventType;
+    NSString* instanceName;
+    NSString* screenTitle;
+    NSString* eventId;
     NSDictionary* eventData;
     NSData *returnData;
     NSString *returnString;
@@ -82,18 +77,25 @@ static NSString * const Tealium_MobileBaseURL = @"https://tags.tiqcdn.com/utag/%
         arguments = [command.arguments objectAtIndex:0];
         eventType = [arguments objectForKey:@"eventType"];
         eventData = [arguments objectForKey:@"eventData"];
+        instanceName = [arguments objectForKey:@"instance"];
+        eventId = [eventData objectForKey:@"link_id"];
+        screenTitle = [eventData objectForKey:@"screen_title"];
         returnData = [NSJSONSerialization dataWithJSONObject:eventData options:NSJSONWritingPrettyPrinted error:nil];
         returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
     }
     @catch (NSException *exception) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Tealium: One of the arguments provdied to the track command is invalid."];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         return;
     }
     
     
     if (eventType != nil && [eventType length] > 0) {
-        [Tealium trackCallType:eventType customData:eventData object:nil];
+        if ([eventType isEqualToString:@"view"]) {
+            [[Tealium instanceForKey:instanceName] trackViewWithTitle:screenTitle dataSources:eventData];
+        } else if ([eventType isEqualToString:@"link"]) {
+            [[Tealium instanceForKey:instanceName] trackEventWithTitle:eventId dataSources:eventData];
+        }
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:returnString];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
