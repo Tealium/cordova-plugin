d3a0cce21e7c3996118090c441ce05deb99b68c6
James Keith <james.keith@tealium.com>
2021-06-08 09:34:14 +0100

    Ios/2.0.0 (#45)
    * wip
    
    * listeners
    
    * tidy
    
    * tidy
    
    * TealiumPlugin update
    
    * removed parens
    
    * Ios/pr fixes (#46)
    
    * rename sample + subspecs
    
    * remove old modules
    
    * missing ios platform

--- TealiumAdIdentifier/src/ios/tealium_adidentifier.h
+++
@@ -1,9 +0,0 @@
-
-#import <Cordova/CDV.h>
-#import <AdSupport/AdSupport.h>
-
-@interface TealiumPgAdIdentifier : CDVPlugin
-
--(void) setPersistent:(CDVInvokedUrlCommand*)command;
--(void) setVolatile:(CDVInvokedUrlCommand*)command;
-@end
